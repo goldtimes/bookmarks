f867ba8087be6c905569deebc8c10b1e2dd6778f
goldtimes <1019825699@qq.com>
2019-08-21 09:36:22 +0800

    用户注册

--- account/forms.py
+++ account/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth.models import  User
 """
 该表单用于对用户进行验证
 PasswordInput微件显示其HTML input元素，同时包含type="password"属性
@@ -8,3 +9,16 @@ class LoginForm(forms.Form):
     username=forms.CharField()
     password =forms.CharField(widget=forms.PasswordInput)
 
+#创建一个表单，用户可以在此输入用户名、真实姓名以及密码
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(label='Password',widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Repeat Password',widget=forms.PasswordInput )
+
+    class Meta:
+        model = User
+        fields = ('username','first_name','email')
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError("Passwords don't match.")
+        return cd['password']
--- account/views.py
+++ account/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate,login
-from .forms import LoginForm
+from .forms import LoginForm,UserRegistrationForm
 # Create your views here.
 from django.contrib.auth.decorators import login_required
 
@@ -35,3 +35,24 @@ def user_login(request):
 @login_required
 def dashboard(request):
     return render(request,'account/dashboard.html',{'section':'dashboard'})
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            #创建一个新的user对象但是并没有存储它
+            new_user = user_form.save(commit=False)
+            #设置用户密码
+            new_user.set_password(user_form.cleaned_data['password'])
+            #保存对象
+            new_user.save()
+            return render(request,'account/register_done.html',{'new_user':new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request,'account/register.html',{'user_form':user_form})
+
+
+
+
+
+
